614df2aa04d43819798d2f2088ef58a71568bfa7
fluttertip <122120987+fluttertip@users.noreply.github.com>
2023-01-24 21:27:55 +0545

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,39 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:o2openai/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-final ThemeData customlighttheme = ThemeData(
-//custom theme for the container and text
+class LoginScreen extends StatefulWidget {
+  bool? darkmode;
 
-  brightness: Brightness.light,
-  scaffoldBackgroundColor: Colors.grey[300],
-  appBarTheme: const AppBarTheme(
-      backgroundColor: Color.fromARGB(255, 220, 220, 222),
-      foregroundColor: Colors.black),
-  //elevated button
-  elevatedButtonTheme: ElevatedButtonThemeData(
-    style: ButtonStyle(
-      backgroundColor:
-          MaterialStateProperty.all<Color>(Color.fromARGB(255, 186, 17, 102)),
-      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+  LoginScreen({super.key, this.darkmode});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController namecontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Center(
+          child: Column(
+        children: [
+          textformfield(
+            hinttext: "Name",
+            controller: namecontroller,
+            textinputtype: TextInputType.emailAddress,
+            obscuretext: false,
+            prefixicon: Icons.email,
+            suffixicon: Icons.visibility,
+            suffixicononpressed: () {},
+          ),
+          textformfield(
+            hinttext: "Password",
+            controller: passwordcontroller,
+            textinputtype: TextInputType.visiblePassword,
+            obscuretext: true,
+            prefixicon: Icons.lock,
+            suffixicon: Icons.visibility,
+            suffixicononpressed: () {},
+          ),
+          //login button
+          ElevatedButton(
+            onPressed: () async {
+              if (namecontroller.text == "admin" &&
+                  passwordcontroller.text == "admin") {
+                SharedPreferences sp = await SharedPreferences.getInstance();
+                sp.setBool("islogin", true);
+                sp.setString("name", 'ADMIN');
+                //go to runapp
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          MyApp(darkmode: widget.darkmode!, islogin: true),
+                    ));
+              } else {}
+            },
+            child: const Text("Login"),
+          ),
+        ],
+      )),
+    );
+  }
+}
+
+Widget textformfield({
+  required String? hinttext,
+  required TextEditingController? controller,
+  required TextInputType? textinputtype,
+  required bool? obscuretext,
+  required IconData? prefixicon,
+  required IconData? suffixicon,
+  required Function? suffixicononpressed,
+}) {
+  return TextFormField(
+    key: UniqueKey(),
+    validator: (value) {
+      if (value!.isEmpty) {
+        return "Please enter some text";
+      }
+      return null;
+    },
+    controller: controller,
+    keyboardType: textinputtype,
+    obscureText: obscuretext!,
+    decoration: InputDecoration(
+      hintText: hinttext,
+      prefixIcon: Icon(prefixicon),
+      suffixIcon: IconButton(
+        onPressed: suffixicononpressed as void Function()?,
+        icon: Icon(suffixicon),
+      ),
     ),
-  ),
-);
-////
-final ThemeData customdarktheme = ThemeData(
-  brightness: Brightness.dark,
-  scaffoldBackgroundColor: const Color.fromARGB(255, 52, 53, 65),
-  appBarTheme: const AppBarTheme(
-      backgroundColor: Color.fromARGB(255, 62, 63, 75),
-      foregroundColor: Colors.white),
-  elevatedButtonTheme: ElevatedButtonThemeData(
-    style: ButtonStyle(
-      backgroundColor:
-          MaterialStateProperty.all<Color>(Color.fromARGB(210, 186, 17, 101)),
-      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-    ),
-  ),
-);
-
-// rgba(62,63,75,255)
-// rgba(52,53,65,255)
-// rgba(247,247,248,255)
-// rgba(255,255,255,255)
+  );
+}
